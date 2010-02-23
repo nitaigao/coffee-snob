f1324af75d0912320c6f1a9c8f522ee418c88c7c
Mark H <markh@trafficbroker-trafficbrokers-macbook-pro.local>
2010-02-23 13:17:08 +0000

    added annotationview

--- CoffeeSnob/Classes/CoffeeMap.m
+++ CoffeeSnob/Classes/CoffeeMap.m
@@ -15,21 +15,63 @@
 	[super init];
 	sortedShops = [[NSMutableArray alloc]init];
 	coffeeShopsWithDistance = [[NSMutableDictionary alloc]init];
-	
+	//map.delegate = self;
 	return self;
 }
 
 
 - (void)showUserLocation {
-	mapView.showsUserLocation = YES;
+	map.showsUserLocation = YES;
 }
 
-- (void)addCoffeeShops:(NSArray*)coffeeShops {		
+- (MKAnnotationView *)mapView:(MKMapView *)mapView
+			viewForAnnotation:(id <MKAnnotation>)annotation {
+    
+	// If it's the user location, just return nil.
+    if ([annotation isKindOfClass:[MKUserLocation class]])
+        return nil;
+
+	// Try to dequeue an existing pin view first.
+	MKPinAnnotationView* pinView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"CustomPinAnnotation"];
+	
+	if (!pinView)
+	{
+		// If an existing pin view was not available, create one
+		pinView = [[[MKPinAnnotationView alloc] initWithAnnotation:annotation
+												   reuseIdentifier:@"CustomPinAnnotation"]
+				   autorelease];
+		pinView.pinColor = MKPinAnnotationColorRed;
+		pinView.animatesDrop = YES;
+		pinView.canShowCallout = YES;
+		
+		// Add a detail disclosure button to the callout.
+		UIButton* rightButton = [UIButton buttonWithType:
+								 UIButtonTypeDetailDisclosure];
+		[rightButton addTarget:self action:@selector(myShowDetailsMethod:)
+			  forControlEvents:UIControlEventTouchUpInside];
+		pinView.rightCalloutAccessoryView = rightButton;
+	}
+	else
+		pinView.annotation = annotation;
+	
+	return pinView;
+
+}
+
+-(IBAction)myShowDetailsMethod{
+	NSLog(@"Here");
+}
+
+- (void)addCoffeeShops:(NSArray*)coffeeShops {
+	
+	//need better place for this....
+	map.delegate = self;
+	
 	for(int i = 0; i < coffeeShops.count; i++) {
 		
 		CoffeeShop* shop = [coffeeShops objectAtIndex:i];
 		
-		double distanceFromUser = [shop.location getDistanceFrom:mapView.userLocation.location];
+		double distanceFromUser = [shop.location getDistanceFrom:map.userLocation.location];
 		
 		if (distanceFromUser < 0 ) {
 			distanceFromUser = distanceFromUser * -1;
@@ -49,19 +91,19 @@
 	NSNumber* bestCoffeeShopKey = [sortedShops objectAtIndex:0];
 	NSString* bestCoffeeShopKeyString = [NSString stringWithFormat:@"%@", bestCoffeeShopKey];
 	CoffeeShop* bestCoffeeShop = [coffeeShopsWithDistance objectForKey:bestCoffeeShopKeyString];
-
-	CoffeeShopAnnotation* coffeeShopAnnotation = [bestCoffeeShop getMapAnnotation];
-	[mapView addAnnotation:coffeeShopAnnotation];
-	[[mapView viewForAnnotation:coffeeShopAnnotation]setSelected:TRUE animated:TRUE];
 	
-	double longitudeDifference = (bestCoffeeShop.location.coordinate.longitude - mapView.userLocation.coordinate.longitude);
-	double latitudeDifference = (bestCoffeeShop.location.coordinate.latitude - mapView.userLocation.coordinate.latitude);
+	CoffeeShopAnnotation* coffeeShopAnnotation = [bestCoffeeShop getMapAnnotation];
+	[map addAnnotation:coffeeShopAnnotation];
+	[map selectAnnotation:[map.annotations objectAtIndex:1] animated:YES];
+	
+	double longitudeDifference = (bestCoffeeShop.location.coordinate.longitude - map.userLocation.coordinate.longitude);
+	double latitudeDifference = (bestCoffeeShop.location.coordinate.latitude - map.userLocation.coordinate.latitude);
 	
 	double longitudeDefferenceMidpoint = longitudeDifference / 2;
 	double latitudeDifferenceMidpoint = latitudeDifference / 2;
 	
-	double centerMapLongitude = mapView.userLocation.coordinate.longitude + longitudeDefferenceMidpoint;
-	double centerMapLatitude = mapView.userLocation.coordinate.latitude + latitudeDifferenceMidpoint;
+	double centerMapLongitude = map.userLocation.coordinate.longitude + longitudeDefferenceMidpoint;
+	double centerMapLatitude = map.userLocation.coordinate.latitude + latitudeDifferenceMidpoint;
 	
 	CLLocation* centerMapLocation = [[CLLocation alloc]initWithLatitude:centerMapLatitude longitude:centerMapLongitude];
 	
@@ -73,7 +115,7 @@
 	region.center = centerMapLocation.coordinate;
 	region.span = span;
 	
-	[mapView setRegion:region animated:TRUE];
+	[map setRegion:region animated:TRUE];
 }
 
 @end
